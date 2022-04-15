a3b6e21f77c3186e4bf0f1893df2e98d2d1e893c
austin <business.mocksock@gmail.com>
2022-04-15 17:23:32 -0500

    add documentation

--- application.py
+++ application.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from falsk_sqlalchemy import SQLAlchemy
 
-# Create a new Flask app
+# Create a new Flask app this will start as the 
+# entry point so far
 app = Flask(__name__)
 
-# Set up the SQLAlchemy
+# Set up the SQLAlchemy 
 app.config['SQLACHEMY_DATABASE_URL'] = 'sqlite:////artists.db'
 db = SQLAlchemy(app)
 
-# Define a class for the Artist Table
+# Define a class for the Artist Table so the data may be sorted
 class Artist(db.Model):
     id = db.Column(db.Integer, prinary_key=True)
     name = db.Column(db.String)
     birth_year = db.Column(db.Integer)
     genre = db.Column(db.String)
 
-# Create a table
+# Create a table that data can be stored in
 db.create_all()
