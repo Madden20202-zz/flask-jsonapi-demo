c329e3ad91a4f48dd45edbb8980978aa1a95c9d0
austin <business.mocksock@gmail.com>
2022-04-15 17:22:04 -0500

    Finish basic set up

--- application.py
+++ application.py
@@ -1,3 +1,19 @@
 from flask import Flask
 from falsk_sqlalchemy import SQLAlchemy
 
+# Create a new Flask app
+app = Flask(__name__)
+
+# Set up the SQLAlchemy
+app.config['SQLACHEMY_DATABASE_URL'] = 'sqlite:////artists.db'
+db = SQLAlchemy(app)
+
+# Define a class for the Artist Table
+class Artist(db.Model):
+    id = db.Column(db.Integer, prinary_key=True)
+    name = db.Column(db.String)
+    birth_year = db.Column(db.Integer)
+    genre = db.Column(db.String)
+
+# Create a table
+db.create_all()
