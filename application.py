2bbff938082c955a1bace02e63fb6998c3c7cfd0
austin <business.mocksock@gmail.com>
2022-04-17 08:49:38 -0500

    Add base code for abstraction layer

--- application.py
+++ application.py
@@ -18,3 +18,19 @@ class Artist(db.Model):
 
 # Create a table that data can be stored in
 db.create_all()
+
+# Abstraction layer
+from marshmallow_jsonapi.flask import Schema 
+from marshmallow_jsonapi import fields
+
+class ArtistSchema(Schema):
+    class Meta:
+        type_ = 'artist'
+        self_view = 'artist_one'
+        self_view_kwargs = ('id' '<id>')
+        self_view_many = 'artist_many'
+
+        id = fields.Integer()
+        name = fields.Str(required=True)
+        birth_year = fields.Integer(load_only=True)
+        genre = fields.Str()
